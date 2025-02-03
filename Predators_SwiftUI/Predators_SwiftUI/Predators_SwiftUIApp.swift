ae148fd70ad04ab46a20ee88794016c7f96c465c
Akshay Ganesh <akshayiyerg@gmail.com>
2025-02-02 23:55:36 -0500

    Initial commit of the project

---
+++ Predators_SwiftUI/Predators_SwiftUI/Predators_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Predators_SwiftUIApp.swift
+//  Predators_SwiftUI
+//
+//  Created by Akshay  on 2025-02-02.
+//
+
+import SwiftUI
+
+@main
+struct Predators_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
