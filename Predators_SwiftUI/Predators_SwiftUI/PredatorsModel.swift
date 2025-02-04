ade57364e82dcb13d024a631f7b48a9cc6ed8553
Akshay Ganesh <akshayiyerg@gmail.com>
2025-02-03 23:43:41 -0500

    Main screen deisgn added UI elemnets

--- Predators_SwiftUI/Predators_SwiftUI/PredatorsModel.swift
+++ Predators_SwiftUI/Predators_SwiftUI/PredatorsModel.swift
@@ -5,20 +5,42 @@
 //  Created by Akshay  on 2025-02-03.
 //
 
+import SwiftUI
 
-struct PredatorsModel: Decodable {
+struct PredatorsModel: Decodable, Identifiable {
     let id: Int
     let name: String
-    let type: String
+    let type: APType
     let latitude, longitude: Double
     let movies: [String]
     let movieScenes: [MovieScene]
     let link: String
     
+    var image: String {
+        name.lowercased().replacingOccurrences(of: " ", with: "")
+    }
+    
     struct MovieScene: Decodable {
         let id: Int
         let movie: String
         let sceneDescription: String
+    }
+    
+    enum APType: String, Decodable {
+        case land
+        case air
+        case sea
+        
+        var background: Color {
+            switch self {
+            case .land:
+                return .brown
+            case .air:
+                return .teal
+            case .sea:
+                return .blue
+            }
+        }
     }
 
 }
