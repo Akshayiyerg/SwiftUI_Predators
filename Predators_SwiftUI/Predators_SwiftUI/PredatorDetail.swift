202879d189f2773fb6f94ef9271d136b45784161
Akshay Ganesh <akshayiyerg@gmail.com>
2025-02-06 00:07:22 -0500

    Added Geometry modifier to scale for all the view and few other modifiers like offset and scaleEffect

--- Predators_SwiftUI/Predators_SwiftUI/PredatorDetail.swift
+++ Predators_SwiftUI/Predators_SwiftUI/PredatorDetail.swift
@@ -11,28 +11,34 @@ struct PredatorDetail: View {
     let predators: PredatorsModel
     
     var body: some View {
-        ScrollView {
-            ZStack {
-                // Background Image
-                Image(predators.type.rawValue)
-                    .resizable()
-                    .scaledToFit()
+        GeometryReader { geo in
+            ScrollView {
+                ZStack(alignment: .bottomTrailing) {
+                    // Background Image
+                    Image(predators.type.rawValue)
+                        .resizable()
+                        .scaledToFit()
+                    
+                    // Dino Image
+                    Image(predators.image)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: geo.size.width/1.5, height: geo.size.height/3.7)
+                        .scaleEffect(x: -1)
+                        .shadow(color: .black, radius: 7)
+                        .offset(y: 20) // this creates the 3d effect. Due to this dino legs is out of image
+                }
                 
-                // Dino Image
-                Image(predators.image)
-                    .resizable()
-                    .scaledToFit()
+                // Dino Name
+                
+                // Current location
+                
+                // Appears in
+                
+                // Movie moments
+                
+                // Link webpage
             }
-            
-            // Dino Name
-            
-            // Current location
-            
-            // Appears in
-            
-            // Movie moments
-            
-            // Link webpage
         }
         .ignoresSafeArea()
     }
